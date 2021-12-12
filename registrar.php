2bdd9a04ef9ce9cd1e6fe95394fd0b54bba34ce4
Alba-Gaitan <78813450+Alba-Gaitan@users.noreply.github.com>
2021-12-12 18:27:52 -0300

    Update registrar

--- registrar.php
+++ registrar.php
@@ -8,10 +8,12 @@
     
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Historia clinica Digital:Ingresa ahora y realiza todos tus tramites medicos desde un solo lugar, a traves de unos pocos click en nuestra plataforma">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
     <meta name="keywords" content="sistema de hospital, software de salud, historia clinica digital, cordoba argentina, telemedicina">
     <link rel="Icon" href="./imagenes/iconmedicina.ico">
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
 
     <title>Ingreso al sistema </title>
     <link rel="stylesheet" href="estilos.css">
