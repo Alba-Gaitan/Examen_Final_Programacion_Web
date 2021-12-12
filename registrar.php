b8b43d7667827d7d176cb3b0ca02bfd37b6edcfb
Alba-Gaitan <78813450+Alba-Gaitan@users.noreply.github.com>
2021-12-12 18:26:12 -0300

    Update registrar

--- registrar.php
+++ registrar.php
@@ -3,6 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    
+     <!-- Etiquetas Metas -->  
+    
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Historia clinica Digital:Ingresa ahora y realiza todos tus tramites medicos desde un solo lugar, a traves de unos pocos click en nuestra plataforma">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
