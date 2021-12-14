25bd87b76a02b07d56d171cc7db03b3e965a5941
Alba-Gaitan <78813450+Alba-Gaitan@users.noreply.github.com>
2021-12-14 00:04:19 -0300

    Update

--- JS/funcionesGrupo99.js
+++ JS/funcionesGrupo99.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
   document.getElementById("formulario").addEventListener('submit', validarFormulario); 
    
  });
- 
+
  function validarFormulario(evento) {
    evento.preventDefault();
    var usuario = document.getElementById('user').value;
@@ -11,14 +11,18 @@ document.addEventListener("DOMContentLoaded", function() {
    var nac = document.getElementById('Nacionalidad').value;
    var provincia = document.getElementById('Provincia').value;
    
-   if(usuario.length == 0) {
+   if(usuario.length ===0) {
      alert('Ingresa tu Nombre y Apellido');
      return;
    }
    if(correo.length === 0) {
     alert('Ingresa tu email');
-    return;
-  }
+      return;
+    } 
+     if (!emailRegex.test(campo.value)){
+     alert('Ingresa un email valido');
+       return;
+     } 
       
    if(fechaNac.length === 0) {
      alert('Ingresa tu fecha de nacimiento');
@@ -32,7 +36,6 @@ document.addEventListener("DOMContentLoaded", function() {
     alert('Ingresa tu provincia');
     return;
   }
-  
     var clave = document.getElementById('clave').value;
     if (clave.length < 6) {
      alert('La clave no es válida');
@@ -43,25 +46,30 @@ document.addEventListener("DOMContentLoaded", function() {
      alert('La clave no es válida');
      return;
   }
-    this.submit();{
+
+  this.submit();{
     alert('Se ha registrado con exito');
   }
  }
+  
 
-// validar email
+//Validacion de email registro y login
 
 document.getElementById('correo').addEventListener('input', function() {
+  
   campo = event.target;
   valido = document.getElementById('emailOK');
+
       
   emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
-
+  evento.preventDefault();
   if (emailRegex.test(campo.value)) {
-
+   
      } else {
     alert('ingrese un email valido');
-  }
-});
+     }  
+  }); 
+
 
  // Funcion calcular edad
 
